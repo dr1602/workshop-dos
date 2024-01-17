dbd790237e71868ebbd8e580849f773eb1cd18ca
dr1602 <david.arangelg@gmail.com>
2024-01-17 09:49:25 -0600

    Lazy Loading

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { registerImage } from './lazy'
 
 // 1. Mover imagenes hacia JS
 // 2. Que las imagenes las use JS
@@ -16,7 +17,7 @@ const createImageNode = () => {
     const imagen = document.createElement('img');
     imagen.className = 'mx-auto rounded-lg';
     imagen.width = '320'
-    imagen.src = `https://randomfox.ca/images/${random()}.jpg` //Todo
+    imagen.dataset.src = `https://randomfox.ca/images/${random()}.jpg` //ToDo
 
     container.appendChild(imagen);
 
@@ -32,62 +33,10 @@ const addButton = document.querySelector('button');
 const addImage = () => {
     const newImage = createImageNode();
     mountNode.append(newImage);
+    registerImage(newImage);
 }
 
 addButton.addEventListener("click", addImage); 
 
 mountNode.appendChild(nuevaImagen);
 
-
-
-
-
-// // llamamos la URL de la API
-// const baseUrl = 'https://randomfox.ca'
-
-// // seleccionar el nodo al que se anexarán
-// const appNode = document.querySelector('#images');
-
-// // 
-
-// window
-//     .fetch(`${baseUrl}/images`)
-//     // procesar la respuesta y convertirla en JSON
-//     .then((respuesta) => respuesta.json())
-//     // JSON -> Data -> Renderizar información
-//     .then((responseJson) => {
-//         const allItems = [];
-
-//         // const image = document.createElement('img');
-//         // image.src = item.image;
-
-//         // image.className = 'mx-auto rounded-xl w-[320px]';
-//         // image.alt = 'Author -> Made by: xinitrc';
-
-//         // const imageDiv = document.createElement('div');
-//         // imageDiv.append(image);
-//         // imageDiv.className = 'p-6'
-
-//         responseJson.data.fetch((item) => {
-//             console.log(item.image);
-
-//             const image = document.createElement('img');
-//             image.src = item.image;
-
-//             image.className = 'mx-auto rounded-xl w-[320px]';
-//             image.alt = 'Author -> Made by: xinitrc';
-
-//             const imageDiv = document.createElement('div');
-//             imageDiv.append(image);
-//             imageDiv.className = 'p-6'
-
-//             // const buttonAdd = document.createElement('button')
-//             // buttonAdd.id = 'addButton';
-
-//             allItems.push(imageDiv)
-
-//         });
-
-//         appNode.append(...allItems);
-
-//     })
